dd386079708e2244f22fc71e99e8565dac2ae123
Daniil Rubin <danyarubin@gmail.com>
2024-01-06 18:51:44 +0000

    WIP mlflow

--- train.py
+++ train.py
@@ -1,6 +1,8 @@
 import logging
+import subprocess
 
 import hydra
+import mlflow
 from omegaconf import DictConfig, OmegaConf
 
 from mlops.dvc import load_data
@@ -14,16 +16,28 @@ def main(cfg: DictConfig):
     log.setLevel(logging.DEBUG)
     log.debug(OmegaConf.to_yaml(cfg))
 
+    experiment = mlflow.set_experiment(cfg.mlflow.experiment)
+
+    mlflow.autolog()
+
     df = load_data(cfg.dvc.path, cfg.dvc.remote)
     X_train, y_train = prepare(df, cfg.dataset.features, cfg.dataset.target)
 
     params = OmegaConf.to_container(cfg.model.params)
     model = LGBM(params)
-    model.fit(X_train, y_train)
 
-    log.debug(model.predict(X_train))
+    with mlflow.start_run(experiment_id=experiment.experiment_id):
+        mlflow.log_param("GIT_COMMIT_HASH", get_commit_hash())
+        model.fit(X_train, y_train)
+
+        log.debug(model.predict(X_train))
 
     model.dump(cfg.model.dump_path)
+
+
+def get_commit_hash():
+    s = subprocess.check_output("git rev-parse HEAD", shell=True).decode()
+    return s.strip()
 
 
 if __name__ == "__main__":
